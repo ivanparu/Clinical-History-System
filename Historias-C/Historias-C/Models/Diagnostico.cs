96e1408e7a761d6e3f09315660ed05ee9bfab9bd
Juan Manuel Tiscornia <juanmanuel.tiscornia6@gmail.com>
2023-09-05 16:26:28 -0300

    Se agregan propiedades a entidad Diagnostico/Epicrisis.

--- Historias-C/Historias-C/Models/Diagnostico.cs
+++ Historias-C/Historias-C/Models/Diagnostico.cs
@@ -2,5 +2,29 @@
 {
     public class Diagnostico
     {
+      int id { get; set; }
+      Epicrisis epicrisis { get; }
+      String descripcion { get; }
+      String recomendacion { get; }   
+
+
+        public Diagnostico (int id,Epicrisis epicrisis, String descripcion, String recomendacion)
+        {
+            this.id = id;   
+            this.epicrisis = epicrisis;   
+            this.descripcion = descripcion;
+            this.recomendacion = recomendacion;
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- Historias-C/Historias-C/Models/Epicrisis.cs
+++ Historias-C/Historias-C/Models/Epicrisis.cs
@@ -2,5 +2,24 @@
 {
     public class Epicrisis
     {
+        int id { get; set; }
+        Episodio episodio;
+        Medico medico { get; }
+        DateTime fechaYHora { get; }
+        Diagnostico diagnostico { get; }
+                        
+
+        public Epicrisis(int id,Episodio episodio, Medico  medico, DateTime fechaYHora, Diagnostico diagnostico)
+        {
+            this.id = id;   
+            this.episodio = episodio;
+            this.medico = medico;   
+            this.diagnostico = diagnostico;
+            this.fechaYHora = fechaYHora;
+
+        }
+
+
+
     }
 }
