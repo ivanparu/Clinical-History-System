b643e07890e1243b90921629e60d6e081ed15bb7
Ivan Parunov <ivanparu04@gmail.com>
2023-09-11 14:28:10 -0300

    Arreglo contructor de Medico

--- Historias-C/Historias-C/Models/Medico.cs
+++ Historias-C/Historias-C/Models/Medico.cs
@@ -5,7 +5,7 @@
 
         public string Matricula { get; set; }
         public string Especialidad { get; set; }
-        public Medico(string nombre, string apellido, int dni, int telefono, string direccion, string matricula, string especialidad) : base(nombre, apellido, dni, telefono, direccion)
+        public Medico(string nombre, string apellido, int dni, int telefono, string direccion, int id,string matricula, string especialidad) : base(nombre, apellido, dni, telefono, direccion, id)
         {
             this.Matricula = matricula;
             this.Especialidad = especialidad;
