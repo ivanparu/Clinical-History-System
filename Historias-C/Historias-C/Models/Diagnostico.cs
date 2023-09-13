cc58a76f39fd25e66dc58b8a45c58523d2dc90ff
Juan Manuel Tiscornia <juanmanuel.tiscornia6@gmail.com>
2023-09-13 18:07:32 -0300

    avanzo en mi brunch, todavia no al main.

--- Historias-C/Historias-C/Models/Diagnostico.cs
+++ Historias-C/Historias-C/Models/Diagnostico.cs
@@ -2,5 +2,29 @@
 {
     public class Diagnostico
     {
+        public int id { get; set; }
+        public Epicrisis epicrisis { get; set; }
+        public String descripcion { get; set; }
+        public String recomendacion { get; set; }
+
+
+        public Diagnostico(int id, Epicrisis epicrisis, String descripcion, String recomendacion)
+        {
+            this.id = id;
+            this.epicrisis = epicrisis;
+            this.descripcion = descripcion;
+            this.recomendacion = recomendacion;
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- Historias-C/Historias-C/Models/Epicrisis.cs
+++ Historias-C/Historias-C/Models/Epicrisis.cs
@@ -2,5 +2,32 @@
 {
     public class Epicrisis
     {
+        public int Id { get; set; } 
+        public Episodio Episodio { get; set;}
+        public Medico Medico { get; set;}
+        public DateTime fechaYHora { get; set;} = DateTime.Now;
+        public Diagnostico Diagnostico { get; set;}
+
+        public Epicrisis()
+        {
+
+        }
+
+        public Epicrisis(int id, Episodio episodio, Medico medico, DateTime fechaYHora, Diagnostico diagnostico)
+        {
+            this.Id = id;
+            this.Episodio = episodio;
+            this.Medico = medico;
+            this.fechaYHora = fechaYHora;
+            this.Diagnostico = diagnostico;
+
+
+        }
+
+
+
+
+
+
     }
 }
