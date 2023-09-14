a6a1734d66fed3017a142b9b9bba014b80dd9f04
augustodoldan <augusto.doldan@hotmail.com>
2023-09-14 14:48:37 -0300

    Se agregan relaciones y restricciones del entregable 2.

--- Historias-C/Historias-C/Models/Evolucion.cs
+++ Historias-C/Historias-C/Models/Evolucion.cs
@@ -1,13 +1,29 @@
-﻿namespace Historias_C.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Historias_C.Models
 {
     public class Evolucion
     {
+        private const string _reqMsg = "El campo {0} es requerido.";
+
+        private const string _reqRange = "El texto debe tener entre {2} y {1} caracteres.";
+
         public int Id { get; set; }
         public Medico Medico { get; set; }
+        [Required(ErrorMessage = _reqMsg)]
+        [DataType(DataType.DateTime)]
         public DateTime FechaYHoraInicio { get; set; }
+        [Required(ErrorMessage = _reqMsg)]
+        [DataType(DataType.DateTime)]
         public DateTime FechaYHoraAlta { get; set; }
+        [Required(ErrorMessage = _reqMsg)]
+        [DataType(DataType.DateTime)]
         public DateTime FechaYHoraCierre { get; set; }
+        [Required(ErrorMessage = _reqMsg)]
+        [StringLength(1000, MinimumLength = 50, ErrorMessage = _reqRange)]
         public string DescripcionAtencion { get; set; }
+        [Required(ErrorMessage = _reqMsg)]
+
         public bool EstadoAbierto { get; set; }
         public List<Notas> Notas { get; set; }
     }
--- Historias-C/Historias-C/Models/Notas.cs
+++ Historias-C/Historias-C/Models/Notas.cs
@@ -1,11 +1,23 @@
-﻿namespace Historias_C.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Historias_C.Models
 {
     public class Notas
     {
+        private const string _reqMsg = "El campo {0} es requerido.";
+
+        private const string _reqRange = "El texto debe tener entre {2} y {1} caracteres.";
         public int Id { get; set; }
         public Evolucion Evolucion { get; set; }
+        public int EvolucionId { get; set; }
         public Empleado Empleado { get; set; }
+       
+        [Required(ErrorMessage = _reqMsg)]
+        [StringLength(400, MinimumLength =2, ErrorMessage = _reqRange)]
         public string Mensaje { get; set; }
+
+        [Required(ErrorMessage = _reqMsg)]
+        [DataType(DataType.DateTime)]
         public DateTime FechaYHora { get; set; }
     }
 }
