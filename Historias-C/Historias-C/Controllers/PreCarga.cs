a38805ce53c2001e16a52a0fe3b8e0f3ddc171ec
augustodoldan <augusto.doldan@hotmail.com>
2023-11-22 19:35:44 -0300

    Se agregan models a la precarga.

--- Historias-C/Historias-C/Controllers/PreCarga.cs
+++ Historias-C/Historias-C/Controllers/PreCarga.cs
@@ -32,12 +32,40 @@ namespace Historias_C.Controllers
             if (!_context.Pacientes.Any())
             {
                  await AddPacientesAsync();
+                await AddOtroPacienteAsync();
+
             }
 
             if (!_context.Medicos.Any())
             {
                 await AddMedicosAsync();
             }
+            if (!_context.Empleados.Any())
+            {
+                await AddOtherEmpleadosAsync();
+            }
+            if (!_context.Evoluciones.Any())
+            {
+                await addEvolucion();
+            }
+            if (!_context.Episodios.Any())
+            {
+                await addEpisodion();
+            }
+
+            if (!_context.Epicrisis.Any())
+            {
+                await addEpicrisis();
+            }
+
+            if (!_context.Notas.Any())
+            {
+                await addNota();
+            }
+
+
+
+
 
 
             return RedirectToAction("Index", "Home", new { mensaje = "se ejecutó la pre-carga" });
@@ -47,10 +75,11 @@ namespace Historias_C.Controllers
         {
             Medico medico1 = new Medico()
             {
+                Id= 40,
                 Especialidad = Especialidad.ALERGIA_E_INMUNOLOGIA,
                 Matricula = "S1212NM",
                 Email = "joacom@ort.edu.ar",
-                UserName = "joacom",
+                UserName = "joacom@ort.edu.ar",
                 Password = Configs.PasswordDef,
                 Nombre = "Joaco",
                 Apellido = "Messi",
@@ -69,8 +98,9 @@ namespace Historias_C.Controllers
         {
             Paciente paciente1 = new Paciente()
             {
+                Id=41,
                 ObraSocial = ObraSocial.OSDE,
-                UserName = "sergiol",
+                UserName = "sergiol@ort.edu.ar",
                 Password = Configs.PasswordDef,
                 Email = "sergiol@ort.edu.ar",
                 Nombre = "Sergio",
@@ -85,6 +115,37 @@ namespace Historias_C.Controllers
                 
                 HistoriaClinica hc = new HistoriaClinica()
                 {
+                    Id = 1212,
+                    PacienteId = paciente1.Id,
+                };
+                _context.Add(hc);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+        private async Task AddOtroPacienteAsync()
+        {
+            Paciente paciente1 = new Paciente()
+            {
+                Id = 3,
+                ObraSocial = ObraSocial.OSDE,
+                UserName = "martinrod@ort.edu.ar",
+                Password = Configs.PasswordDef,
+                Email = "martinrod@ort.edu.ar",
+                Nombre = "Martín",
+                Apellido = "Rodriguez",
+                DNI = 34765444,
+                Telefono = 1132446565,
+            };
+            var resultadoNewPaciente = await _userManager.CreateAsync(paciente1, Configs.PasswordDef);
+            if (resultadoNewPaciente.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(paciente1, Configs.PacienteRolName);
+
+                HistoriaClinica hc = new HistoriaClinica()
+                {
+                    Id = 1213,
                     PacienteId = paciente1.Id,
                 };
                 _context.Add(hc);
@@ -96,7 +157,8 @@ namespace Historias_C.Controllers
         {
             Empleado empleado1 = new Empleado()
             {
-                UserName = "mariomartinez",
+                Id= 1,
+                UserName = "mariom@ort.edu.ar",
                 Password = Configs.PasswordDef,
                 Email = "mariom@ort.edu.ar",
                 Nombre = "Mario",
@@ -110,6 +172,107 @@ namespace Historias_C.Controllers
                 await _userManager.AddToRoleAsync(empleado1, Configs.EmpleadoRolName);
                 await _context.SaveChangesAsync();
             }
+        }
+
+        private async Task AddOtherEmpleadosAsync()
+        {
+            Empleado empleado2 = new Empleado()
+            {
+                Id = 2,
+                UserName = "santiagor@ort.edu.ar",
+                Password = Configs.PasswordDef,
+                Email = "santiagor@ort.edu.ar",
+                Nombre = "Santiago",
+                Apellido = "Rinaudo",
+                DNI = 29344727,
+                Telefono = 132550299,
+            };
+            var resultadoNewEmpleado = await _userManager.CreateAsync(empleado2, Configs.PasswordDef);
+            if (resultadoNewEmpleado.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(empleado2, Configs.EmpleadoRolName);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task addEpisodion()
+        {
+            Episodio episodio = new Episodio()
+            {
+                Id = 27,
+                Motivo = "Suba de la presión arterial.",
+                Descripcion = "Parámetros del corazon alterados.",
+                HistoriaClinicaId = 1212,
+                EmpleadoId= 1
+            };
+
+            Episodio episodioCerrado = new Episodio()
+            {
+                Id = 28,
+                Motivo = "Descenso de la presión arterial.",
+                Descripcion = "Poca presion sanguinea.",
+                HistoriaClinicaId = 1213,
+                FechaYHoraCierre = DateTime.Now,
+                EstadoAbierto = false,
+                EmpleadoId = 1
+
+            };
+
+            _context.Add(episodio);
+            _context.Add(episodioCerrado);
+
+            await _context.SaveChangesAsync();
+        }
+            private async Task addEvolucion()
+        {
+            Evolucion evolucion = new Evolucion()
+            {
+                Id = 12,
+                MedicoId = 40,
+                EpisodioId = 28,
+                DescripcionAtencion = "El paciente evoluciona favorablemente.",
+                FechaYHoraCierre= DateTime.Now,
+                EstadoAbierto = false
+
+            };
+
+            _context.Add(evolucion);
+            await _context.SaveChangesAsync();
+            
+        }
+
+        private async Task addEpicrisis()
+        {
+            Epicrisis epicrisis = new Epicrisis()
+            {
+                Id = 22,
+                MedicoId = 40,
+                EpisodioId = 28,
+                Descripcion = "Arteria femoral obstruida.",
+                Recomendacion = "Valcote 35mg cada 12 hs"
+
+            };
+
+            _context.Add(epicrisis);
+            await _context.SaveChangesAsync();
+
+        }
+
+        private async Task addNota()
+        {
+            Notas nota = new Notas()
+            {
+                Id = 33,
+                EvolucionId = 12,
+                EmpleadoId = 1,
+                Mensaje = "Arteria femoral obstruida.",
+                FechaYHora = DateTime.Now
+
+            };
+
+            _context.Add(nota);
+            await _context.SaveChangesAsync();
+
         }
 
         private async Task CrearRoles()
